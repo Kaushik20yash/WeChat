0d279936a555e95856398aff22642bd3a0faed72
Kaushik20yash <147990478+Kaushik20yash@users.noreply.github.com>
2025-07-28 12:10:54 +0530

    Update login.js

--- login.js
+++ login.js
@@ -32,7 +32,7 @@ document.getElementById('btnn').addEventListener('click', function(event) {
     .then((userCredential) => {
       alert('Login successful!');
       console.log('Logged in:', userCredential.user);
-      window.location.href = 'index.html';
+      window.location.href = 'index1.html';
     })
     .catch((error) => {
       console.error('Error:', error);
